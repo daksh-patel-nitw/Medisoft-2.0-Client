ea02849f0e2360f7aeeb0d2e03d81c727772ca11
Djpatel00 <dakshjpatel007@gmail.com>
2026-05-04 14:11:41 +0530

    Fixing build error

---
+++ pages/auth/login.jsx
@@ -0,0 +1,14 @@
+// pages/auth/login.jsx
+import React from 'react';
+
+const LoginPage = () => {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <p>Building this soon...</p>
+    </div>
+  );
+};
+
+// This "export default" is the exact thing Next.js is looking for!
+export default LoginPage;
